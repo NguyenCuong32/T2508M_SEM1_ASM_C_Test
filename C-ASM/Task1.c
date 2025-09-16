c188ee68ee7cf4bbd346e2ef21891897aa499511
Xuan Cuong <xcuong286@gmail.com>
2025-09-16 10:02:55 +0700

    Task 1 & Task 2

---
+++ C-ASM/Task1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+float y(float x)
+{
+    float g = 9.8;
+    float result = - 0.5 * g * x * x + x;
+    return result;
+}
+
+int main()
+{
+    float result, x;
+
+    // Test voi x = 5;
+    result = y(5);
+    printf("The position of bullet: %.2f.\n", result);
+
+    // Nhap x tu ban phim;
+    printf("Enter the value for x: ");
+    scanf("%f", &x);
+
+    result = y(x);
+    printf("The position of bullet: %.2f\n", result);
+
+    // Kiem tra vi tri cua vien dan
+    if (y(x) > 50 || y(x) < 30)
+    {
+        printf("The bullet got out.\n");
+    }
+    else
+    {
+        printf("The bullet got a goal.\n");
+    }
+}
---
+++ C-ASM/Task2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void findMinMax (int array[], int *max)
+{
+    *max = array[0];
+    for (int i = 0; i < 6; i++)
+    {
+        if (array[i] > *max)
+        {
+            *max = array[i];
+        }
+    }
+}
+
+int main()
+{
+    printf("Task 2.1\n");
+    int array[6] = {7, 6, 3, 34, 10, 710};
+    printf("a. Using for loop\n");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("The element %d of the array is %d\n", i, array[i]);
+    }
+    printf("\n");
+
+    printf("b. Using while loop\n");
+    int i = 0;
+    while (i < 6)
+    {
+        printf("The element %d of the array is %d\n", i, array[i]);
+        i++;
+    }
+    printf("\n");
+
+    printf("c. Using do-while loop.\n");
+    int j = 0;
+    do 
+    {
+        
+        printf("The element %d of the array is %d\n", j, array[j]);
+        j++;
+    }
+    while (j < 6);
+    printf("\n");
+
+    printf("Task 2.2\n");
+    int max;
+    findMinMax(array, &max);
+    printf("The largest value is: %d\n", max);
+
+    return 0;
+}
