f79ea23b61be32c32d722d41a7f9276e1c660f3b
Thanhtung-dt <thanhtungdv30@gmail.com>
2025-09-16 09:50:14 +0700

    baitap2

--- testC/baitap2.c
+++ testC/baitap2.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(){
     int array[6] = {7,6,3,34,10,710};
-    for (int i = 0;i < 6;i++){
-        printf("phần tử phần %d : %d\n",i + 1,array[i]);
+    int i =0;
+    while (i < 6){
+        printf("phần tủ của %d: %d\n",i + 1,array[i]);
+        i++;
     }
 }
